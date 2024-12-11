d75e281bd9e78190afced268ed80b27acfb11d58
TalesPires <145354273+TalesPires@users.noreply.github.com>
2024-12-11 10:26:54 -0300

    aaaa

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,19 @@
+#!/bin/bash
+
+# Activate Python runtime provided by Vercel
+if command -v python3 &>/dev/null; then
+    alias python=python3
+elif command -v python &>/dev/null; then
+    alias python=python
+else
+    echo "Python is not available. Aborting."
+    exit 1
+fi
+
+# Install required dependencies
+python -m ensurepip --upgrade || echo "ensurepip not supported"
+python -m pip install --upgrade pip setuptools wheel
 pip install -r requirements.txt
+
+# Collect static files
 python manage.py collectstatic --noinput
